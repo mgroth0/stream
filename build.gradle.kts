f914f0085dcde18eccb87c7f17069b958d6182b3
mgroth0 <mgroth49@gmail.com>
2022-05-30 19:31:01 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import matt.klib.str.upper
+
 /*NPM INSTALL TASK IS DISABLED IN ROOT BUILDSCRIPT BECAUSE IT PRODUCES OBNOXIOUS WARNING. WILL NEED TO ENABLE THAT TO INSTALL DEPENDENCIES PROBABLY*/
 
 //dependencies {
@@ -15,7 +17,11 @@ configure<org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension> {
   sourceSets {
 	val commonMain by getting {
 	  dependencies {
-		implementation(projects.k.klib)
+		if (rootDir.name.upper() == "FLOW") {
+		  implementation(project(":k:klib"))
+		} else {
+		  implementation("matt.k:klib:+")
+		}
 	  }
 	}
   }
