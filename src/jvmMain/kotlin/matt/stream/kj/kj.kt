ddb47cbacbb6aec3d154c0bfe11f0c9ded81c8dc
mgroth0 <mgroth49@gmail.com>
2022-06-06 04:19:58 -0400

    autocommit

--- src/jvmMain/kotlin/matt/stream/kj/kj.kt
+++ src/jvmMain/kotlin/matt/stream/kj/kj.kt
@@ -4,43 +4,43 @@ import kotlinx.coroutines.delay
 import java.io.Reader
 import java.lang.System.currentTimeMillis
 
-@Throws(EndOfStreamException::class)
-fun Reader.readWithTimeout(timeoutMillis: Int): String {
-  val entTimeMS = currentTimeMillis() + timeoutMillis
-  var r = ""
-  var c: Int
-  while (currentTimeMillis() < entTimeMS) {
-	if (ready()) {
-	  c = read()
-	  if (c == -1) {
-		if (r.isNotEmpty()) return r else throw EndOfStreamException()
-	  }
-	  r += c.toChar().toString()
-	}
-  }
-  return r
-}
+//@Throws(EndOfStreamException::class)
+//fun Reader.readWithTimeout(timeoutMillis: Int): String {
+//  val entTimeMS = currentTimeMillis() + timeoutMillis
+//  var r = ""
+//  var c: Int
+//  while (currentTimeMillis() < entTimeMS) {
+//	if (ready()) {
+//	  c = read()
+//	  if (c == -1) {
+//		if (r.isNotEmpty()) return r else throw EndOfStreamException()
+//	  }
+//	  r += c.toChar().toString()
+//	}
+//  }
+//  return r
+//}
+//
+//@Throws(EndOfStreamException::class)
+//fun Reader.readLineWithTimeout(timeoutMillis: Int): String {
+//  val entTimeMS = currentTimeMillis() + timeoutMillis
+//  var r = ""
+//  var c: Int
+//  while (currentTimeMillis() < entTimeMS) {
+//	if (ready()) {
+//	  c = read()
+//	  when {
+//		c == '\n'.code            -> return r
+//		c == -1 && r.isNotEmpty() -> return r
+//		c == -1                   -> throw EndOfStreamException()
+//	  }
+//	  r += c.toChar().toString()
+//	}
+//  }
+//  return r
+//}
 
-@Throws(EndOfStreamException::class)
-fun Reader.readLineWithTimeout(timeoutMillis: Int): String {
-  val entTimeMS = currentTimeMillis() + timeoutMillis
-  var r = ""
-  var c: Int
-  while (currentTimeMillis() < entTimeMS) {
-	if (ready()) {
-	  c = read()
-	  when {
-		c == '\n'.code            -> return r
-		c == -1 && r.isNotEmpty() -> return r
-		c == -1                   -> throw EndOfStreamException()
-	  }
-	  r += c.toChar().toString()
-	}
-  }
-  return r
-}
-
-class EndOfStreamException: Exception()
+//class EndOfStreamException: Exception()
 
 @Suppress("BlockingMethodInNonBlockingContext")
 suspend fun Reader.readLineOrSuspend(suspendMillis: Long): String? {
@@ -50,7 +50,7 @@ suspend fun Reader.readLineOrSuspend(suspendMillis: Long): String? {
 	if (ready()) {
 	  c = read()
 	  when {
-		c == '\n'.code            -> return r
+		c == '\n'.code /*10*/     -> return r
 		c == -1 && r.isNotEmpty() -> return r
 		c == -1                   -> return null
 	  }
