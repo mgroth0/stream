6402f73f6a7408398fa2cdb178b32e003eac25d1
mgroth0 <mgroth49@gmail.com>
2022-06-12 16:30:55 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,15 +7,23 @@ plugins {
   kotlin("multiplatform")
 }
 
+
+//implementations(
+//  ":k:klib"
+//)
+
+
 configure<org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension> {
   sourceSets {
 	val commonMain by getting {
 	  this.dependencies {
 		implementation(libs.kotlinx.coroutines)
+
 		if (rootDir.name.upper() == "FLOW") {
 		  implementation(project(":k:klib"))
 		} else {
-		  implementation("matt.k:klib:+")
+		  todo("this is bad")
+		  implementation("matt.flow:klib:+")
 		}
 	  }
 	}
