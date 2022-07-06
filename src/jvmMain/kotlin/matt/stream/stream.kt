c671a25027955189ec93dd4039ecd064413cb745
mgroth0 <mgroth49@gmail.com>
2022-07-06 12:24:39 -0400

    autocommit

---
+++ src/jvmMain/kotlin/matt/stream/stream.kt
@@ -0,0 +1,38 @@
+package matt.stream
+
+import matt.stream.ReaderEndReason.TYPE.END_OF_STREAM
+import matt.stream.ReaderEndReason.TYPE.IO_EXCEPTION
+import java.io.IOException
+import java.io.Reader
+
+class ReaderEndReason(val type: TYPE, val exception: Exception? = null) {
+  enum class TYPE {
+	END_OF_STREAM,
+	IO_EXCEPTION
+  }
+}
+
+fun Reader.forEachChar(op: (String)->Unit): ReaderEndReason {
+  var s: String
+  var c: Int
+  try {
+	while (true) {
+	  c = read()
+	  if (c == -1) {
+		return ReaderEndReason(type = END_OF_STREAM)
+	  }
+	  s = c.toChar().toString()
+	  if (s.isNotEmpty()) op(s)
+	}
+  } catch (e: IOException) {
+	return ReaderEndReason(type = IO_EXCEPTION, exception = e)
+  }
+}
+
+fun Process.forEachOutChar(op: (String)->Unit) = inputStream.bufferedReader().forEachChar {
+  op(it)
+}
+
+fun Process.forEachErrChar(op: (String)->Unit) = errorStream.bufferedReader().forEachChar {
+  op(it)
+}
