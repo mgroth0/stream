88dcd8d884fd1802792003a15215f11324387b84
Matthew Groth <matthewgroth@Matthews-MacBook-Pro.local>
2022-08-06 17:58:54 -0400

    autocommit

--- src/jvmMain/kotlin/matt/stream/encoding/encoding.kt
+++ src/jvmMain/kotlin/matt/stream/encoding/encoding.kt
@@ -1,38 +1,66 @@
 package matt.stream.encoding
 
 import matt.klib.lang.err
+import matt.klib.lang.untilIs
+import matt.klib.log.HasLogger
+import matt.klib.log.Logger
 import java.io.InputStream
 import java.net.SocketTimeoutException
 
-fun InputStream.encodingReader(encoding: Encoding) = EncodingReader(encoding, this)
+fun InputStream.encodingReader(encoding: Encoding, log: Logger) = EncodingReader(encoding, this, log)
 
 class EncodingReader(
   private val encoding: Encoding,
   private val input: InputStream,
-) {
+  log: Logger
+): HasLogger(log) {
   private val br by lazy { input.bufferedReader() }
+
+  fun section(): ReadSectionResult = decorate {
+	var sect = ""
+	untilIs {
+	  when (val c = char()) {
+
+		is ReadChar  -> sect += c.c
+
+		DELIM        -> ReadSection(sect)
+
+		EOF, TIMEOUT -> c.also {
+		  require(sect.isEmpty()) {
+			"got $c within a section:\"$sect\""
+		  }
+		}
+	  }
+	}
+  }
+
   fun char(): ReadCharResult = try {
 	when (val c = br.read()) {
 	  -1                      -> EOF
 	  encoding.delimiter.code -> DELIM
 	  encoding.escape.code    -> when (val cc = br.read()) {
 		-1                      -> err("received EOF in escape sequence...")
-		encoding.delimiter.code -> ResultChar(encoding.delimiter)
+		encoding.delimiter.code -> ReadChar(encoding.delimiter)
 		else                    -> err("invalid escape: $cc")
 	  }
 
-	  else                    -> ResultChar(c.toChar())
+	  else                    -> ReadChar(c.toChar())
 	}
   } catch (e: SocketTimeoutException) {
 	TIMEOUT
   } /*add any other timeout exceptions here as well and return TIMEOUT*/
 }
 
+sealed interface ReadSectionResult
+
+@JvmInline
+value class ReadSection(val sect: String): ReadSectionResult
 
 sealed interface ReadCharResult
 
 @JvmInline
-value class ResultChar(val c: Char): ReadCharResult
+value class ReadChar(val c: Char): ReadCharResult
 object DELIM: ReadCharResult
-object EOF: ReadCharResult
-object TIMEOUT: ReadCharResult
+object EOF: ReadCharResult, ReadSectionResult
+object TIMEOUT: ReadCharResult, ReadSectionResult
+
