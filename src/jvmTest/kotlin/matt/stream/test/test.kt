f6f68af66679ea44e11598552641bb24b8a83715
mgroth0 <mgroth49@gmail.com>
2023-10-04 11:50:06 -0400

    changed src/jvmTest/kotlin/matt/stream/test/test.kt

--- src/jvmTest/kotlin/matt/stream/test/test.kt
+++ src/jvmTest/kotlin/matt/stream/test/test.kt
@@ -4,7 +4,7 @@ package matt.stream.test
 import matt.stream.ReaderEndReason
 import matt.stream.ReaderEndReason.TYPE
 import matt.stream.ReaderEndReason.TYPE.END_OF_STREAM
-import matt.test.JupiterTestAssertions.assertRunsInOneMinute
+import matt.test.assertions.JupiterTestAssertions.assertRunsInOneMinute
 import kotlin.test.Test
 
 class StreamTests {
